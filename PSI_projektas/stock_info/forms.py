c68a0e380e878ecf3e95a013c9dcafe3a35cf978
Tautvydas <petkus09@TAUTVYDO.(none)>
2013-11-24 22:45:51 +0200

    labas

--- PSI_projektas/stock_info/forms.py
+++ PSI_projektas/stock_info/forms.py
@@ -1,10 +1,12 @@
 from django.forms.forms import Form
-from django.forms.fields import FileField
+from django.forms.fields import FileField, DateField, CharField
 from django.utils.translation import ugettext_lazy
+from django.forms.widgets import DateInput
 
 class FileForm(Form):
     file_name = FileField(label=ugettext_lazy('File name'))
 
 class QualityForm(Form):
     date_from = DateField(label=ugettext_lazy('Date from'))
-    date_to = DateField(label=ugettext_lazy('Date from'))
+    date_to = DateField(label=ugettext_lazy('Date to'))
+    group = CharField(label=ugettext_lazy('Group'))
